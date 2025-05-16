d5d92546160cb985e744814d5cf8d48a12670d6c
Diego <da.rosado64@gmail.com>
2025-05-15 20:04:31 -0400

    Index.js commit update

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Heroku!');
+  res.send('Testing in app Heroku!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on the port ${PORT}`);
 });
